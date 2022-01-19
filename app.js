f9f81fc2bce05a6e4fea05e5f06ab4c695055be4
juliengriffonwinwin <julien.griffon@win-win.com>
2022-01-19 16:25:18 +0100

    3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const app = express() 
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World 2')
+    res.send('Hello World 3')
 })
 
 app.get('/hello', (req, res) => {
     res.json({message: 'Hello World'})
 })
 
-const PORT = process.env.PORT || 8081 
+const PORT = process.env.PORT || 8081
 
 app.listen(PORT, () => {
     console.log('Server is running...')
